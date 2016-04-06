3212fe11211f50e14f790576c3f42787426bc49a
Logan <crandall.logan@gmail.com>
2016-04-06 17:43:08 -0400

    v1.0

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+** file: js/main.js
+** description: javascript code for "html/main.html" page
+*/
+
+$(function() {
+	var elem = $('.btn.btn-green.js-action-button');
+	console.log(elem);
+	elem.click();
+	window.close();
+});
